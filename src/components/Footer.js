bc7d3844c72e9cd0a052cf4a3c9e20e2a501832c
faraz-dev99 <arbazuddin242830@gmail.com>
2024-06-07 14:41:07 +0530

    change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer(){
     const year=new Date().getFullYear();
     return (
-        <footer className=' flex justify-center self-start 
+        <footer className='absolute bottom-0 w-full flex justify-center self-start 
         '>
             <p className=' text-center'>copyright &copy; {year} FarazKeep all rights reserved</p>
         </footer>
